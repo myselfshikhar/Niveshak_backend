0fb3567625a853728e7a097f43facc459b9c7ba6
Shikhar Katiyar <148053313+myselfshikhar@users.noreply.github.com>
2025-07-17 00:43:27 +0530

    Auth verification  commit

--- index.js
+++ index.js
@@ -19,10 +19,15 @@ const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/AuthRoute");
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:8000",  "https://niveshak-frontend.vercel.app",
-    "https://niveshak-dashboard.vercel.app/"], // allow frontend and dashboard
-  credentials: true // this is required for cookies/session
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "https://niveshak-frontend.vercel.app",
+    "https://niveshak-dashboard.vercel.app" // ✅ NO trailing slash!
+  ],
+  credentials: true,
 }));
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
